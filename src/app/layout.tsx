d23a0e209f5ea3593b35df2482e2c887ae697e5a
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-03-17 21:00:03 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import AuthProvider from "@/providers/AuthProvider";
 const inter = Epilogue({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sketchy: Collaborative Whiteboard App",
+  title: "Sketchy: Collaborative Whiteboard App - Work Together in Real Time",
   description: "Sketchy is a modern whiteboard application that enables real-time collaboration, allowing multiple users to sketch, draw, and brainstorm together.",
   keywords: ["whiteboard", "sketch", "drawing", "collaboration", "real-time", "Next.js", "Konva.js", "typescript"],
   // website: "https://www.Sketchy.com",
